f4f06661f5f6dbafa438b5a9455122086251e46a
Karan Sharma <karan1276@gmail.com>
2016-01-17 22:36:01 +0530

    adding angular template

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uniApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the uniApp
+ */
+angular.module('uniApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uniApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uniApp
+ */
+angular.module('uniApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
